561b3169b2aeb859c2a23ed76253ee44fccf2a1b
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2020-07-20 18:38:43 +0700

    doing

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'gatsby';
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/products">Products</Link></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import { Link } from 'gatsby';
+
+// components
+import Navbar from '../components/Navbar';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Navbar />
+      Hello people`!
+      <Link to="/blog/"> blog page </Link>
+    </div>
+  )
 }
